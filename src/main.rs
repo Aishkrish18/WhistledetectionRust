59072ba0c7735085bc49c506ea760f243e12386e
Aishkrish18 <aishu2.k@gmail.com>
2022-01-22 13:07:13 +0100

    recording into audio buffer

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ enum Event<'a> {
 struct State {
     /// Temporary buffer for holding real-time audio samples.
     buffer: Audio<Mono32>,
+    buffer_length: usize
 }
 
 impl State {
@@ -21,10 +22,10 @@ impl State {
     fn event(&mut self, event: Event<'_>) {
         match event {
             Event::Record(microphone) => {
-                //println!("Recording");
+                println!("Recording");
                 self.buffer.extend(microphone);
-                if self.buffer.len() >= 48_000 * 10 {
-                    write_pcm(&self.buffer);
+                if self.buffer.len() >= self.buffer_length {
+                    println!("Finished");
                     std::process::exit(0);
                 }
             }
@@ -32,24 +33,15 @@ impl State {
     }
 }
 
-/// Save a Raw PCM File from an audio buffer.
-fn write_pcm(buffer: &Audio<Mono32>) {
-    let mut pcm: Vec<u8> = Vec::new();
-    for frame in buffer.iter() {
-        let sample: f32 = frame.channels()[0].into();
-        pcm.extend(sample.to_le_bytes().iter());
-    }
-    std::fs::write("pcm.raw", pcm.as_slice()).expect("Failed to write file");
-}
+
 
 /// Program start.
 fn main() {
     let mut state = State {
         buffer: Audio::with_silence(48_000, 0),
+        buffer_length: (48000.0 * 1.5) as usize
     };
     let mut microphone = Microphone::default();
-
-    exec!(state.event(wait! {
-        Event::Record(microphone.record().await),
+    exec!(state.event(wait!{Event::Record(microphone.record().await),
     }))
 }
